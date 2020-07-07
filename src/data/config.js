c359641bc974a2ec61db21747dd33f65963f3a64
Ben Lirio <benjlirio@gmail.com>
2020-07-07 13:34:12 -0400

    all files

---
+++ src/data/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  defaultTitle: 'Ben Lirio',
+  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  author: 'Ben Lirio',
+  url: 'https://benlirio.com',
+  legalName: 'Benjamin Lirio',
+  defaultDescription: 'I’m Ben and I’m a Software engineer!',
+  socialLinks: {
+    github: 'https://github.com/BenLirio/',
+    linkedin: 'www.linkedin.com/in/benjamin-lirio',
+  },
+  googleAnalyticsID: 'G-DZ37BB6T3L',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+  },
+  address: {
+    city: 'Hudsonville',
+    region: 'Region',
+    country: 'US',
+    zipCode: '49426',
+  },
+  contact: {
+    email: 'benjlirio@gmail.com',
+    phone: '616-215-9313',
+  },
+  foundingDate: '2019',
+};
